7d0e8af6d2c9962e65fb5266557e5be024363673
21stfloor <21stfloordev@gmail.com>
2023-10-17 11:40:29 +0800

    working, lcd, ticket logic. remaining servo

--- weighing_scale_ardunio.ino
+++ weighing_scale_ardunio.ino
@@ -5,21 +5,25 @@
 HX711_ADC LoadCell(A0, A1); // dt pin 6, pin 7;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 const uint8_t TICKET_IN_PIN = A2;
+const uint8_t BUTTON_PIN = 8; // Digital pin for the push button
 
-void setup()
-{
+boolean isDispensing = false; // Variable to track ticket dispensing status
+
+void setup() {
   LoadCell.begin();
   LoadCell.start(2000);
   LoadCell.setCalFactor(999.0);
   lcd.init();
   lcd.backlight();
-  pinMode(TICKET_IN_PIN, INPUT);
+  pinMode(TICKET_IN_PIN, INPUT); // Set TICKET_IN_PIN as an output
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Set BUTTON_PIN as input with pull-up resistor
 }
 
-void loop()
-{
+void loop() {
   LoadCell.update();
   float i = LoadCell.getData();
+  lcd.setCursor(5, 0);
+  lcd.print("Button: " + String(digitalRead(BUTTON_PIN)));
   lcd.setCursor(5, 1);
   lcd.print("Weight[g]:");
   lcd.setCursor(5, 2);
@@ -31,10 +35,19 @@ void loop()
   lcd.setCursor(5, 3);
   lcd.print("Ticket Count: " + String(ticketCount));
 
-  if(ticketCount > 0){
-    analogWrite(TICKET_IN_PIN, 0);
-    delay(300 * ticketCount);
-    analogWrite(TICKET_IN_PIN, 255);
-    delay(5000);
-}
+  
 
+  if (digitalRead(BUTTON_PIN) == LOW && !isDispensing && ticketCount > 0) {
+    while (ticketCount > 0) {
+        isDispensing = true;
+        analogWrite(TICKET_IN_PIN, 0);
+        delay(300);        
+        analogWrite(TICKET_IN_PIN, 255);
+        ticketCount--;
+        delay(300);
+    }
+    
+    delay(3000);
+    isDispensing = false; // Reset the dispensing status
+  }
+}
